c141aecd71daeb2694a7fe611d40262591c57b11
vagrant <vagrant@lucid32.(none)>
2011-11-09 20:55:41 -0800

    basics without less or bootstrap

--- settings/common.py
+++ settings/common.py
@@ -1,42 +1,41 @@
 # -*- coding: utf-8 -*-
 # Django settings for temp project.
 
+import sys
 import os.path
 import posixpath
-import pinax
 
-PINAX_ROOT = os.path.abspath(os.path.dirname(pinax.__file__))
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+## Directories
+SETTINGS_DIRECTORY = os.path.dirname( os.path.abspath(__file__) )
+PROJECT_ROOT = os.path.dirname( SETTINGS_DIRECTORY )
 
-# tells Pinax to use the default theme
-PINAX_THEME = "default"
+# Has db and all that stuff in it, the non-tracked things
+SITE_ROOT = os.path.dirname( PROJECT_ROOT )
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+sys.path.append(SITE_ROOT)
+sys.path.append(PROJECT_ROOT + '/apps')
 
-# tells Pinax to serve media through the staticfiles app.
-SERVE_MEDIA = DEBUG
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(SITE_ROOT, "media")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = "/media/"
+
+# Absolute path to the directory that holds static files like app media.
+# Example: "/home/media/media.lawrence.com/apps/"
+STATIC_ROOT = os.path.join(SITE_ROOT, "static")
+
+# URL that handles the static files like app media.
+# Example: "http://media.lawrence.com"
+STATIC_URL = "/static/"
+
 
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-ADMINS = [
-    # ("Your Name", "your_email@domain.com"),
-]
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "dev.db",                       # Or path to database file if using sqlite3.
-        "USER": "",                             # Not used with sqlite3.
-        "PASSWORD": "",                         # Not used with sqlite3.
-        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -49,33 +48,13 @@ TIME_ZONE = "US/Eastern"
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = "en-us"
 
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media")
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = "/site_media/media/"
-
-# Absolute path to the directory that holds static files like app media.
-# Example: "/home/media/media.lawrence.com/apps/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
-
-# URL that handles the static files like app media.
-# Example: "http://media.lawrence.com"
-STATIC_URL = "/site_media/static/"
-
 # Additional directories which hold static files
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "media"),
-    os.path.join(PINAX_ROOT, "media", PINAX_THEME),
+    os.path.join(PROJECT_ROOT, "static-files"),
 ]
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
@@ -83,8 +62,6 @@ STATICFILES_DIRS = [
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = "s65mx86vc)u-d!^+#w@u8ktofgf^vlxz5y(x=uk8%h9r)lyr@r"
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = [
@@ -106,7 +83,6 @@ ROOT_URLCONF = "temp.urls"
 
 TEMPLATE_DIRS = [
     os.path.join(PROJECT_ROOT, "templates"),
-    os.path.join(PINAX_ROOT, "templates", PINAX_THEME),
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
