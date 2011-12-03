276cec4ce8f362c453dffe5cbc95ad2d44dad459
Adam Cezar Jenkins <emperorcezar@gmail.com>
2011-12-02 20:51:48 -0600

    Fixing path on development database settings

--- settings/development.py
+++ settings/development.py
@@ -4,7 +4,7 @@ The idea is to set your settings to settings.development
 in development and settings.production in production.
 This allows you to overwrite anything in common.py
 '''
-
+import os
 from common import *
 
 DEBUG = True
@@ -13,14 +13,13 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": PROJECT_ROOT + "db/dev.db",                       # Or path to database file if using sqlite3.
+        "NAME": os.path.join(SITE_ROOT, "db", "dev.db"),                       # Or path to database file if using sqlite3.
         "USER": "",                             # Not used with sqlite3.
         "PASSWORD": "",                         # Not used with sqlite3.
         "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
         "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
     }
 }
-
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
